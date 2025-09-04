d9408f7c0a00edfc19f3f02539a4902ba854486d
vieriseo0925 <syoungho00@naver.com>
2025-09-04 11:22:10 +0900

    1. 블록과 스코프 ### 블록 *block*
    - 0개 이상의 문 *statement* 들을 묶은 단위
    - 제어문, 함수, 클래스 등에 사용
    - 새로운 **스코프** 생성

---
+++ src/Sec06/Chap01/Ex01/Ex01.java
@@ -0,0 +1,38 @@
+package Sec06.Chap01.Ex01;
+// 💡{ } 블록 생성
+public class Ex01 {
+    public static void main(String[] args) {
+        {
+            int x = 1;
+            System.out.println(x);
+        }
+
+        {
+            int intNum = 123;
+            String str = "블록 밖은 위험해";
+        }
+//    💡 블록 안에서 선언된 것은 밖에서 사용 불가
+        intNum = 234;
+        System.out.println(str);
+
+        String x = "전국구 보스";
+
+        {
+            String y = "동네 양아치";
+
+//        💡 블록 안쪽에서는 바깥의 것 사용 가능
+            System.out.println(x);
+            System.out.println(y);
+        }
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int z = 1;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(z + i);
+        }
+        System.out.println(i); // ⚠️ 불가
+    }
+}
