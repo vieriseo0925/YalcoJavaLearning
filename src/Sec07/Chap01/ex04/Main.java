00cd41babf47c7c12e87af12d01bc339ab52d8db
vieriseo0925 <syoungho00@naver.com>
2025-11-25 20:55:02 +0900

    1. 블록과 스코프 ### 블록 *block*
    - 0개 이상의 문 *statement* 들을 묶은 단위
    - 제어문, 함수, 클래스 등에 사용
    - 새로운 **스코프** 생성

---
+++ src/Sec07/Chap01/ex04/Main.java
@@ -0,0 +1,10 @@
+package Sec07.Chap01.ex04;
+
+public class Main {
+    public static void main(String[] args) {
+        NotCloneable notCloneable = new NotCloneable(
+                "클릭들 1", 1, new int[] {1, 2, 3},
+                new
+        )
+    }
+}
